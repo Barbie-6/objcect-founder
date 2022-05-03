d9b5499d4bf8aee939cdb1af7662f927fc699f59
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2022-05-03 19:51:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ if(status != ""){
     for(i=0; i<objects.length; i++){
         document.getElementById("status").innerHTML = "Objects Detected";
         stroke("red");
+        noFill();
         strokeWeight(2);
         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         text(objects[i].label, objects[i].x + 15, objects[i].y + 15 );
